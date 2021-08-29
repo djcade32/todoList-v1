ab25e3f7711b29a34ab704d5acdaaf4a8e961770
Norman <norman_cade32@yahoo.com>
2021-08-29 11:44:54 -0400

    setup heroku

--- app.js
+++ app.js
@@ -124,6 +124,12 @@ app.get("/:newListName", function (req, res) {
   });
 });
 
-app.listen(3000, function () {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
+
+app.listen(port, function () {
   console.log("Server Has Started");
 });
